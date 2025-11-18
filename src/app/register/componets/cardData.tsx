f7f4771233362e8ee6b2887d5eeb592c519c079a
Daniel <xdaniel945x@gmail.com>
2025-11-17 23:27:57 -0600

    add country with choose cellphone number code

--- src/app/register/componets/cardData.tsx
+++ src/app/register/componets/cardData.tsx
@@ -46,21 +46,43 @@ export function Card() {
     
     const nameRef = useRef<HTMLInputElement>(null)
     const ageRef = useRef<HTMLInputElement>(null)
+    const emailRef = useRef<HTMLInputElement>(null)
+    const cellRef = useRef<HTMLInputElement>(null)
+    const countryRef = useRef<HTMLInputElement>(null)
+    const userRef = useRef<HTMLInputElement>(null)
+    const dirRef = useRef<HTMLInputElement>(null)
     const divRef = useRef<HTMLInputElement>(null)
     const divRef1 = useRef<HTMLInputElement>(null)
     const divRef2 = useRef<HTMLInputElement>(null)
+    const divRef3 = useRef<HTMLInputElement>(null)
+    const divRef4 = useRef<HTMLInputElement>(null)
+    const divRef5 = useRef<HTMLInputElement>(null)
+    const divRef6 = useRef<HTMLInputElement>(null)
+
+    const [country, Setcountry] = useState('')
     
     const nameRegex = new RegExp("^[A-Za-z]+([\\s][A-Za-z]+)*$")
-    const emailRegex = new RegExp("")
+    const emailRegex = new RegExp("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$")
+    const cellRegex = new RegExp("^[0-9]{0,}$")
+    const dirRegex = new RegExp("^[A-Za-z]+#[0-9]{1,}[A-Za-z]{0,}$")
+    const userRegex = new RegExp("^[A-Za-z0-9]([A-Za-z0-9_]*[A-Za-z0-9])?$")
 
     const handleChange = (e : ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
+        
         const {name, value} = e.target
 
         const inputName = nameRef.current
         const inputAge = ageRef.current
+        const inputEmail = emailRef.current
+        const inputCell = cellRef.current
+        const inputDir = dirRef.current
         const divName = divRef.current
         const divName1 = divRef1.current
         const divAge = divRef2.current
+        const divEmail = divRef3.current
+        const divCell = divRef4.current
+        const divCell1 = divRef5.current
+        const divDir = divRef6.current
 
         if(name === 'name'){
             if(value.length < 4){
@@ -95,7 +117,61 @@ export function Card() {
                 inputAge?.classList.add('border-violet-400')
             }
         }
-        
+
+        if(name === 'email'){
+            if(emailRegex.test(value) === true){
+                divEmail?.classList.add('hidden')
+                inputEmail?.classList.remove('border-red-400')
+                inputEmail?.classList.add('border-violet-400')
+            }
+            else{
+                divEmail?.classList.remove('hidden')
+                inputEmail?.classList.remove('border-violet-400')
+                inputEmail?.classList.add('border-red-400')
+            }
+        }
+
+        if(name === 'cellphone'){
+            if(cellRegex.test(value) === true){
+                divCell?.classList.add('hidden')
+                inputCell?.classList.remove('border-red-400')
+                inputCell?.classList.add('border-violet-400')
+            }
+            else{
+                divCell?.classList.remove('hidden')
+                inputCell?.classList.remove('border-violet-400')
+                inputCell?.classList.add('border-red-400')
+            }
+            if(value.length < 10){
+                divCell1?.classList.remove('hidden')
+                inputCell?.classList.remove('border-violet-400')
+                inputCell?.classList.add('border-red-400')
+            }
+            else{
+                divCell1?.classList.add('hidden')
+                inputCell?.classList.remove('border-red-400')
+                inputCell?.classList.add('border-violet-400')
+            }
+        }
+
+        if(name === 'direction'){
+            if(dirRegex.test(value.replaceAll(" ", "")) === true){
+                divDir?.classList.add('hidden')
+                inputDir?.classList.remove('border-red-400')
+                inputDir?.classList.add('border-violet-400')
+            }
+            else{
+                divDir?.classList.remove('hidden')
+                inputDir?.classList.add('border-red-400')
+                inputDir?.classList.remove('border-violet-400')
+            }
+        }
+
+        if(name === 'code') {
+            const countryName = new Intl.DisplayNames(['es'], {type: 'region'}).of(value) 
+            Setcountry(countryName + '')
+        }
+
         setDataForm({
             ...dataForm,
             [name]: value
@@ -152,7 +228,7 @@ export function Card() {
                                         placeholder='12-80 years old'
                                         className="ml-3 mb-1 pl-2 p-1 pr-2 bg-white rounded-2xl border-violet-400 border-2 focus:ring-0 focus:outline-none" 
                                     />
-                                    <div className='text-red-400' ref={divRef2}>
+                                    <div className='text-red-400 hidden' ref={divRef2}>
                                         <label className='text-xs'>
                                             To register you must be 12 years of age
                                         </label>
@@ -163,14 +239,20 @@ export function Card() {
                                 <th scope='row'>Email</th>
                                 <th scope='row'>
                                     <input 
+                                        ref={emailRef}
                                         id='email' 
                                         name='email' 
                                         type='email' 
                                         value={dataForm.email} 
                                         onChange={handleChange} 
-                                        className="ml-3 mb-1 pl-2 p-1 pr-2 bg-white rounded-2xl border-violet-400 border-2 focus:outline-violet-800" 
+                                        className="ml-3 mb-1 pl-2 p-1 pr-2 bg-white rounded-2xl border-violet-400 border-2 focus:ring-0 focus:outline-none" 
                                         placeholder="example@gmail.com" 
                                     />
+                                    <div className='text-red-400 hidden' ref={divRef3}>
+                                        <label className='text-xs'>
+                                            Incorrect format, check the example.
+                                        </label>
+                                    </div>
                                 </th>
                             </tr>
                             <tr>
@@ -183,7 +265,7 @@ export function Card() {
                                                 name='code'
                                                 value={dataForm.code} 
                                                 onChange={handleChange} 
-                                                className="ml-3 mb-1 pl-2 p-1 pr-2 bg-white rounded-2xl border-violet-400 border-2 focus:outline-violet-800"
+                                                className="ml-3 mb-1 pl-2 p-1 pr-2 bg-white rounded-2xl border-violet-400 border-2 focus:ring-0 focus:outline-none"
                                             >
                                                 {countryOptions.map(country => (
                                                     <option key={country.isoCode} value={country.value}>
@@ -194,14 +276,25 @@ export function Card() {
                                         </div>
                                         <div>
                                             <input 
-                                                placeholder="Number"
+                                                ref={cellRef}
+                                                placeholder="Cellphone"
                                                 id='cellphone' 
                                                 name='cellphone'
-                                                type="tel" 
+                                                type="tel"
                                                 value={dataForm.cellphone} 
                                                 onChange={handleChange} 
-                                                className="w-34 ml-3 mb-1 pl-2 p-1 pr-2 bg-white rounded-2xl border-violet-400 border-2 focus:outline-violet-800" 
+                                                className="w-34 ml-3 mb-1 pl-2 p-1 pr-2 bg-white rounded-2xl border-violet-400 border-2 focus:ring-0 focus:outline-none" 
                                             />
+                                            <div className='text-red-400 hidden' ref={divRef4}>
+                                                <label className='text-xs'>
+                                                    Only contain numbers
+                                                </label>
+                                            </div>
+                                            <div className='text-red-400 hidden' ref={divRef5}>
+                                                <label className='text-xs'>
+                                                    Length of 10 characters
+                                                </label>
+                                            </div>
                                         </div>
                                     </div>
                                 </td>
@@ -210,23 +303,32 @@ export function Card() {
                                 <th scope='row'>Direction</th>
                                 <th scope='row'>
                                     <input 
+                                        ref={dirRef}
                                         id='direction' 
                                         name='direction'
                                         value={dataForm.direction}
                                         onChange={handleChange} 
-                                        className="ml-3 mb-1 pl-2 p-1 pr-2 bg-white rounded-2xl border-violet-400 border-2 focus:outline-violet-800" 
+                                        className="ml-3 mb-1 pl-2 p-1 pr-2 bg-white rounded-2xl border-violet-400 border-2 focus:ring-0 focus:outline-none" 
+                                        placeholder='plata #76 moroleon'
                                     />
+                                    <div className='text-red-400 hidden' ref={divRef6}>
+                                        <label className='text-xs'>
+                                            Invalid format, check the example
+                                        </label>
+                                    </div>
                                 </th>
                             </tr>
                             <tr>
-                                <th scope='row'>Contry</th>
+                                <th scope='row'>Country</th>
                                 <th scope='row'>
                                     <input 
+                                        ref={countryRef}
                                         id='country' 
                                         name='country'
-                                        value={dataForm.country} 
+                                        value={country} 
                                         onChange={handleChange} 
-                                        className="ml-3 mb-1 pl-2 p-1 pr-2 bg-white rounded-2xl border-violet-400 border-2 focus:outline-violet-800" 
+                                        className="ml-3 mb-1 pl-2 p-1 pr-2 bg-white rounded-2xl border-violet-400 border-2 focus:ring-0 focus:outline-none" 
+                                        disabled
                                     />
                                 </th>
                             </tr>
@@ -238,7 +340,7 @@ export function Card() {
                                         name='usename' 
                                         value={dataForm.usename} 
                                         onChange={handleChange} 
-                                        className="ml-3 mb-1 pl-2 p-1 pr-2 bg-white rounded-2xl border-violet-400 border-2 focus:outline-violet-800" 
+                                        className="ml-3 mb-1 pl-2 p-1 pr-2 bg-white rounded-2xl border-violet-400 border-2 focus:ring-0 focus:outline-none" 
                                     />
                                 </th>
                             </tr>
@@ -251,7 +353,7 @@ export function Card() {
                                         type='password' 
                                         value={dataForm.password} 
                                         onChange={handleChange}
-                                        className="ml-3 mb-2 pl-2 p-1 pr-2 bg-white rounded-2xl border-violet-400 border-2 focus:outline-violet-800" 
+                                        className="ml-3 mb-2 pl-2 p-1 pr-2 bg-white rounded-2xl border-violet-400 border-2 focus:ring-0 focus:outline-none" 
                                     />
                                 </th>
                             </tr>
